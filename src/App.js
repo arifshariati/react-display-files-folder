607d61cc2fadd676186b3c0eb12254b5df53c76e
Arif Shariati <mohammad.arif.fast@hotmail.com>
2022-01-31 16:32:16 +0300

    Added recursion for getting folder and files from json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import { Folder } from './components';
+import json from './data/json';
 
 function App() {
   return (
     <div className="App">
+      <Folder json={json} />
     </div>
   );
 }
