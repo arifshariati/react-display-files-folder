6b3a03cd6f24b11dab743d45ae238013b512a239
Arif Shariati <mohammad.arif.fast@hotmail.com>
2022-01-31 16:18:26 +0300

    Added JSON file

---
+++ src/data/json.js
@@ -0,0 +1,17 @@
+const json = {
+    name: 'root',
+    isFolder: true,
+    items: [
+        {
+            name: 'Public',
+            isFolder: true,
+            items: [
+                {
+                    name: 'package.json',
+                    isFolder: false,
+                }
+            ]
+        }
+    ]
+};
+export default json;
